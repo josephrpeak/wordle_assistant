5b65ed38552668627db9516d148c36a73f0625fc
Joseph Peak <josephrpeak@gmail.com>
2022-03-06 02:21:21 -0600

    added check for no dictionary results

--- wordle.py
+++ wordle.py
@@ -17,7 +17,7 @@ possible = []
 for i in range(5):
 	print("Enter char " + str(i) + ":")
 	letter = str(input())
-	while(letter not in charlist and (letter != "?" and letter != "\s")):
+	while(not letter or letter not in charlist and letter != "?" and letter != "\s"):
 		print("Invalid entry. Try again: ")
 		letter = str(input())
 	current+=letter
@@ -58,24 +58,26 @@ elif(len(unknown) == 4):
 					current[unknown[3]] = charlist[l]
 					permutation.append("".join(current))
 
-
 for guess in permutation:
 	if guess in clean_dictionary:
 		print(guess)
 		possible.append(guess)
 
-while(len(possible) > 1):
-	print("Eliminate letter: ")
-	eliminate = str(input())
+if(len(possible) == 0):
+	print("No results found in dictionary.")
+else:
+	while(len(possible) > 1):
+		print("Eliminate letter: ")
+		eliminate = str(input())
 
-	removed = []
-	for word in possible:
-		for char in word:
-			if(char == eliminate and word not in removed):
-				removed.append(word)
+		removed = []
+		for word in possible:
+			for char in word:
+				if(char == eliminate and word not in removed):
+					removed.append(word)
 
-	possible = [word for word in possible if word not in removed]
-	for word in possible:
-		print(word)
+		possible = [word for word in possible if word not in removed]
+		for word in possible:
+			print(word)
 
-print("Answer: " + str(possible))
+	print("Answer: " + str(possible))
